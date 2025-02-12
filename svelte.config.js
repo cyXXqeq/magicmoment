3ad0a77b87bec3433d6b050d9af8c4a743b09ac9
Дмитрий Макаров <d.makarov@cism-ms.ru>
2025-02-12 23:25:49 +0300

    fix github page

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
-const forGithubPages = false;
+const forGithubPages = true;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
